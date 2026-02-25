c2cf5bf3893b810b205fcc87167ace67ef8e766a
Aarush P <170435164+Aarush0605@users.noreply.github.com>
2026-02-25 10:45:34 +0530

    Delete Day3/convert_temp.cpp

--- Day3/convert_temp.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    vector<double> convertTemperature(double celsius) {
-        double kelvin = celsius + 273.15;
-        double fahrenheit = celsius * 1.80 + 32.00;
-        vector<double> ans;
-        return ans = { kelvin , fahrenheit };
-    }
-};
